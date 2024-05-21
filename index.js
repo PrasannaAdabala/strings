6f1551b2967459d01a9031bbce243b5493c87a1d
PrasannaAdabala <prasannaadabala385@gmail.com>
2024-05-21 11:24:02 +0530

    file

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1.Increase the salary of a person , if person is manager increase by 15% ,
+//  if person is hr increase by 10% ,
+//  if person is dev increase by 20%
+// take base salary of 30000 and print the amount along with type of person
+// let salary=30000;
+// let role;
+// let manager;
+// let hr;
+// let dev;
+// if(role==manager){
+//   console.log(salary*1.15)
+// }
+//  if(role==hr){
+//     console.log(salary*1.10)
+// }
+//   if(role==dev){
+//     console.log(salary*1.20)
+// }
+// else{
+//     console.log("nothing")
+// }
+
+// 2. Marriage eligibility
+//  	if person is male ask for age enter the age and print some message else not allowed
+// 	same thing for female
+// const input=require("prompt-sync")();
+// let age1=input("enter a male age ");
+// let age=input("enter a female age ");
+// if(age1==18){
+//     console.log(" female is allowed")
+// }
+// if(age==21){
+//          console.log("male is allowed")
+
+// }
+// else{
+//     console.log("not allowed")
+// }
+// 2. 0x1234 , 0O345 , 0B10101 , 0B1234 , 0o9 -> find output of these
+// console.log(0x1234) 
+// console.log(0O345 ) 
+// console.log(0B10101 )
+
+
